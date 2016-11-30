61e497a56da42ea5dedd54fd3c259d0d60eceb84
mpietrzyk <mkpietrzyk@gmail.com>
2016-11-30 15:39:17 +0100

    FD5SS - 25 - solution final
    *Added Jquery UI for animation boost.
    *Added rel links for Jquery UI
    *Added rel links for personal JS files.

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,3 +3,10 @@
  */
 
 
+$(document).on('click', 'a[href^="#"]', function(e) {
+  var id = $(this).attr('href');
+  e.preventDefault();
+  var pos = $(id).offset().top;
+  console.log(pos);
+  $('body, html').animate({scrollTop: pos}, 1500, 'easeOutQuad');
+});
