a6e100f44a0393639050f22ece28e6ea6adc587d
mpietrzyk <mkpietrzyk@gmail.com>
2016-11-30 16:52:54 +0100

    FD5SS - 9- solution final

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,12 +1,32 @@
-/**
- * Created by mpietrzyk on 30.11.16.
- */
+var sections = $('section');
+var nav = $('nav');
+var nav_height = nav.outerHeight();
 
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+  console.log(cur_pos);
 
-$(document).on('click', 'a[href^="#"]', function(e) {
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+      bottom = top + $(this).outerHeight();
+
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').parent().removeClass('active');
+      sections.removeClass('active');
+
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').parent().addClass('active');
+    }
+  });
+});
+
+$(document).on('click', 'a[href^="#"]', function (e) {
   var id = $(this).attr('href');
   e.preventDefault();
   var pos = $(id).offset().top;
-  console.log(pos);
   $('body, html').animate({scrollTop: pos}, 1500, 'easeOutQuad');
 });
+
+
+
+
