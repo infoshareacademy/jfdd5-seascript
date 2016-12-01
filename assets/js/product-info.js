4a9a669524b3f02b2307c03815fdec8166d5f4d7
Maciek <mb.maciejborkowski@gmail.com>
2016-12-01 16:50:02 +0100

    dodanie animacji do zdjęć

--- assets/js/product-info.js
+++ assets/js/product-info.js
@@ -10,17 +10,17 @@ $(document).ready(function () {
   function check_if_in_view() {
     var windowHeight = $window.height();
     var windowTopPosition = $window.scrollTop();
-    var window_bottom_position = (windowTopPosition + windowHeight);
+    var windowBottomPosition = (windowTopPosition + windowHeight);
 
     $.each($featurePhotos, function() {
       var $element = $(this);
-      var element_height = $element.outerHeight();
-      var element_top_position = $element.offset().top;
-      var element_bottom_position = (element_top_position + element_height);
+      var elementHeight = $element.outerHeight();
+      var elementTopPosition = $element.offset().top;
+      var elementBottomPosition = (elementTopPosition + elementHeight);
 
       //check to see if this current container is within viewport
-      if ((element_bottom_position >= windowTopPosition) &&
-        (element_top_position <= window_bottom_position)) {
+      if ((elementBottomPosition >= windowTopPosition) &&
+        (elementTopPosition <= windowBottomPosition)) {
         $element.addClass('in-view');
       } else {
         $element.removeClass('in-view');
