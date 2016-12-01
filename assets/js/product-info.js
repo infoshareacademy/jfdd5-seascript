0a975a358d4f4d7851ae832b82f35e99253dedeb
Maciek <mb.maciejborkowski@gmail.com>
2016-12-01 16:37:00 +0100

    dodanie animacji do zdjęć/ustawione zdjęcia (do czyszczenia kod)

--- assets/js/product-info.js
+++ assets/js/product-info.js
@@ -1,28 +1,30 @@
 /**
  * Created by maciej on 30.11.16.
  */
-var $featurePhotos = $('.feature-photo');
-var $window = $(window);
-$window.on('scroll', check_if_in_view);
-$window.trigger('scroll');
 
-function check_if_in_view() {
-  var window_height = $window.height();
-  var window_top_position = $window.scrollTop();
-  var window_bottom_position = (window_top_position + window_height);
+$(document).ready(function () {
+  var $featurePhotos = $('.feature-photo');
+  var $window = $(window);
+  $window.on('scroll load', check_if_in_view);
 
-  $.each($featurePhotos, function() {
-    var $element = $(this);
-    var element_height = $element.outerHeight();
-    var element_top_position = $element.offset().top;
-    var element_bottom_position = (element_top_position + element_height);
+  function check_if_in_view() {
+    var windowHeight = $window.height();
+    var windowTopPosition = $window.scrollTop();
+    var window_bottom_position = (windowTopPosition + windowHeight);
 
-    //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
-      (element_top_position <= window_bottom_position)) {
-      $element.addClass('in-view');
-    } else {
-      $element.removeClass('in-view');
-    }
-  });
-}
+    $.each($featurePhotos, function() {
+      var $element = $(this);
+      var element_height = $element.outerHeight();
+      var element_top_position = $element.offset().top;
+      var element_bottom_position = (element_top_position + element_height);
+
+      //check to see if this current container is within viewport
+      if ((element_bottom_position >= windowTopPosition) &&
+        (element_top_position <= window_bottom_position)) {
+        $element.addClass('in-view');
+      } else {
+        $element.removeClass('in-view');
+      }
+    });
+  }
+});
