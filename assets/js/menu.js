31493dadfe78ace3dc3609d7548dec8916b4bc02
mpietrzyk <mkpietrzyk@gmail.com>
2016-12-01 16:53:54 +0100

    FD5SS -  responsive menu wip :)

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,6 +1,7 @@
 var sections = $('section');
 var nav = $('nav');
 var nav_height = nav.outerHeight();
+var button = nav.find('button')
 
 $(window).on('scroll', function () {
   var cur_pos = $(this).scrollTop();
@@ -10,14 +11,12 @@ $(window).on('scroll', function () {
     nav.find('a').parent().removeClass('active');
     nav.find('a').parent().removeClass('grow');
     nav.find('a').parent().addClass('shrink');
-
   }
   else{
     nav.removeClass('section-look');
     nav.find('a').parent().removeClass('shrink');
     nav.find('a').parent().addClass('grow');
   }
-
 
   sections.each(function() {
     var top = $(this).offset().top - nav_height,
@@ -38,6 +37,14 @@ $(document).on('click', 'a[href^="#"]', function (e) {
   e.preventDefault();
   var pos = $(id).offset().top;
   $('body, html').stop().animate({scrollTop: pos}, 1500, 'easeOutQuad');
+});
+
+$(button).on('click', function(){
+  var li = nav.find('li.menu-link');
+  var ul = nav.find('ul');
+  li.stop().slideToggle();
+  ul.stop().slideToggle();
+  // li.parent().stop().slideToggle();
 });
 
 
