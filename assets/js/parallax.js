914448bc174d380f83d5f54598177efe971399b9
Tomasz Gola <gola92@gmail.com>
2016-12-04 22:12:48 +0100

    paralaxa dodanie fali i  w miare co nie co zrobione

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -7,19 +7,19 @@ $(window).on('scroll', parabole_tanczo);
 function parabole_tanczo() {
 
   $('.fala-1').stop().css({
-    bottom: ($(document).scrollTop()) * 0.5,
-    left: ($(document).scrollTop()) * 0.5
+    bottom: $(document).scrollTop() * 0.5,
+    left: $(document).scrollTop() * 0.5
 
   });
 
   $('.fala-2').stop().css({
-    bottom: ($(document).scrollTop()) * 0.3,
-    right: ($(document).scrollTop()) * 0.3
+    bottom: $(document).scrollTop() * 0.3,
+    right: $(document).scrollTop() * 0.3
   });
 
   $('.fala-3').stop().css({
-    bottom: ($(document).scrollTop()) * 0.15,
-    left: ($(document).scrollTop()) * 0.15
+    bottom: $(document).scrollTop() * 0.15,
+    left: $(document).scrollTop() * 0.15
   });
 
 }
