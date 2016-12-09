a2b4d19ceaf1c4a1ab719ef49eb58e968f17cb65
TomaszGola <gola92@gmail.com>
2016-12-09 16:10:50 +0100

    wstawienie gry do index.html i dodanie kilku znaczących opcji

--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -1,8 +1,25 @@
 /**
  * Created by tgola on 30.11.16.
  */
+function detectmob() {
+  if( navigator.userAgent.match(/Android/i)
+    || navigator.userAgent.match(/webOS/i)
+    || navigator.userAgent.match(/iPhone/i)
+    || navigator.userAgent.match(/iPad/i)
+    || navigator.userAgent.match(/iPod/i)
+    || navigator.userAgent.match(/BlackBerry/i)
+    || navigator.userAgent.match(/Windows Phone/i)
+  ){
+    return true;
+  }
+  else {
+    return false;
+  }
+}
 
-$(window).on('load resize scroll', parabole_tanczo);
+if (detectmob() === false) {
+  $(window).on('load resize scroll', parabole_tanczo);
+}
 
 function parabole_tanczo() {
 
